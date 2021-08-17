7ff38a0734bc186575366f91b9b38e44900c30d2
GitDogBar <Dogbar>
2021-08-18 01:08:15 +0700

    Add project files.

---
+++ Models/DB/GroupMenusTbl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace JoinTable.Models.DB
+{
+    public partial class GroupMenusTbl
+    {
+        public int Id { get; set; }
+        public string GroupNameMenu { get; set; }
+
+        [ForeignKey("Id")]
+        public int GroupMenu { get; set; }
+    }
+}
---
+++ Models/DB/ListMenusTbl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace JoinTable.Models.DB
+{
+    public partial class ListMenusTbl
+    {
+        public int Id { get; set; }
+        public string MenusName { get; set; }
+        public int? GroupMenu { get; set; }
+        public int? Price { get; set; }
+    }
+}
